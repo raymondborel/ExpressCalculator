e991a9987c8cdfab1c6eb417a4c6801ec01385fa
raymondborel <rborel852@gmail.com>
2023-08-15 22:41:45 -0700

    Created subtraction route

--- server.js
+++ server.js
@@ -15,8 +15,22 @@ app.get('/add/:x/:y', (req, res) => {
     }
     const result = x + y;
 
-    res.send(`The some of ${x} and ${y} is ${result}.`)
+    res.send(`The sum of ${x} and ${y} is ${result}.`)
 });
+
+app.get('/subtract/:x/:y', (req, res) => {
+    let x =parseFloat(req.params.x);
+    let y =parseFloat(req.params.y);
+
+    if(isNaN(x) || isNaN(y)) {
+        return res.status(400).send('Invalid number, Please enter a valid number');
+    }
+    let result = x + y;
+
+    res.send(`The difference of ${x} and ${y} is ${result}.`)
+});
+
+
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
